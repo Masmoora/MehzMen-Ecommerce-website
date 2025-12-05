ef36cf9da0f392bb4e4cad99d7ac72252f7fbdb9
Masmoora EK <masmooram@gmail.com>
2025-12-05 11:46:05 +0530

    Added admin login page

---
+++ controllers/admin/adminController.js
@@ -0,0 +1,17 @@
+const User = require("../../models/userSchema");
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+
+
+const loadLogin = async (req, res) => {
+  if (req.session.admin) {
+    return res.redirect('/admin/dashboard')
+  }
+  res.render('admin-login',{message:null})
+}
+
+
+module.exports={
+    loadLogin
+}
---
+++ routes/adminRouter.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const router=express.Router()
+const adminController=require('../controllers/admin/adminController')
+
+router.get('/login',adminController.loadLogin)
+
+
+
+
+
+
+
+
+
+
+
+module.exports=router
