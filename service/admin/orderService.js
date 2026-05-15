562427986a6374f809d0caea10826711b916588b
Masmoora EK <masmooram@gmail.com>
2026-05-15 19:20:03 +0530

    admin side invoice bug fixed

--- service/admin/orderService.js
+++ service/admin/orderService.js
@@ -437,7 +437,7 @@ class AdminOrderService {
     const html = await ejs.renderFile(templatePath, { order });
 
     // 6️⃣ Generate PDF
-    const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({ headless: 'new',args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
 
     await page.setContent(html, { waitUntil: 'networkidle0' });
